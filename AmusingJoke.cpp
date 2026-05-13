fada0c18f7b5d03d3ff909fe1b1640e448477324
Sahil-Gondaliya <gondaliyasahil95@gmail.com>
2026-05-13 23:45:15 +0530

    Add files via upload

---
+++ AmusingJoke.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a,b,c;
+    cin >> a >> b >> c;
+
+    string s = a+b;
+
+    sort(s.begin(), s.end());
+    sort(c.begin(), c.end());
+
+    if (s == c)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    return 0;
+}
