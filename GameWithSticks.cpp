8f79d3e5b1421eb82a49dad3915806b1d0a1a876
Sahil-Gondaliya <gondaliyasahil95@gmail.com>
2026-05-18 18:53:24 +0530

    Add files via upload

---
+++ GameWithSticks.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,m; cin >> n >> m;
+
+    if(min(m,n) % 2 == 0)
+    {
+        cout << "Malvika";
+    }
+    else
+    {
+        cout << "Akshat";
+    }
+    return 0;
+}
