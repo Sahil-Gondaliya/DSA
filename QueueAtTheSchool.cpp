e5adab38994a90ff6888b06bb703a7e80eada78d
Sahil-Gondaliya <gondaliyasahil95@gmail.com>
2026-05-22 15:09:52 +0530

    Add files via upload

---
+++ QueueAtTheSchool.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,t; cin >> n >> t;
+    string s; cin >> s;
+
+    while (t-->0)
+    {
+        for(int i=1; i<n; i++)
+        {
+            if(s[i-1] == 'B' && s[i] == 'G')
+            {
+                swap(s[i-1], s[i]);
+                i++;
+            }
+        }
+    }
+    cout << s;
+    return 0;
+}
