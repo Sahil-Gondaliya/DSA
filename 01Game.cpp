f983ac64107a3254bce1de2c0e7fa77e3e792dc3
Sahil-Gondaliya <gondaliyasahil95@gmail.com>
2026-06-15 00:09:42 +0530

    Add files via upload

---
+++ 01Game.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        string s;
+        cin >> s;
+
+        int zero = 0, one = 0;
+
+        for(char c : s)
+        {
+            if(c == '0')
+                zero++;
+            else
+                one++;
+        }
+
+        if(min(zero, one) % 2)
+            cout << "DA\n";
+        else
+            cout << "NET\n";
+    }
+
+    return 0;
+}
