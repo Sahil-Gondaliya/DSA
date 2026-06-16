bead6df75da74b9cf2c3ecafa1199df1cd22623a
Sahil-Gondaliya <gondaliyasahil95@gmail.com>
2026-06-16 23:00:49 +0530

    Add files via upload

---
+++ LineTrip.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t; cin >> t;
+    while (t-->0)
+    {
+        int n,x; cin >> n >> x;
+        int a[n];
+        for(int i=0; i<n; i++) cin >> a[i];
+
+        int maximum=a[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            if(a[i]-a[i-1] > maximum)
+            {
+                maximum = a[i]-a[i-1];
+            }
+        }
+        
+        if((x - a[n-1])*2 > maximum)
+        {
+            maximum = (x - a[n-1])*2;
+        }
+
+        cout << maximum << endl;
+    }
+    
+    return 0;
+}
